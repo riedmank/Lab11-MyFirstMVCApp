1f6dbee4f9022c882c7cf48693d7248b941c2220
Kristofer Riedman <riedmank@hotmail.com>
2018-10-23 11:44:33 -0700

    added comments and styles to all pages

--- MyFirstMVCApp/Controllers/HomeController.cs
+++ MyFirstMVCApp/Controllers/HomeController.cs
@@ -9,18 +9,34 @@ namespace MyFirstMVCApp.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// This route is the default route to the home page
+        /// </summary>
+        /// <returns>Returns a ViewResult</returns>
         [HttpGet]
         public ViewResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// This post route takes user input and calls the result route
+        /// </summary>
+        /// <param name="begYear">Beginning year</param>
+        /// <param name="endYear">Ending year</param>
+        /// <returns>Returns a redirect</returns>
         [HttpPost]
         public IActionResult Index(int begYear, int endYear)
         {
             return RedirectToAction("Result", new { begYear, endYear});
         }
 
+        /// <summary>
+        /// This method passes the user input to the TimePerson Class file
+        /// </summary>
+        /// <param name="begYear">Beginning year</param>
+        /// <param name="endYear">Ending year</param>
+        /// <returns>Returns a list of people to the results page</returns>
         public ViewResult Result(int begYear, int endYear)
         {
             List<TimePerson> listOfPeople = TimePerson.GetPersons(begYear, endYear);
