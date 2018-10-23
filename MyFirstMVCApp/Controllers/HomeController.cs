005f44765ce3403ab84bacdcfd5c20822bcc247b
Kristofer Riedman <riedmank@hotmail.com>
2018-10-23 10:49:28 -0700

    result page most of the way setup

--- MyFirstMVCApp/Controllers/HomeController.cs
+++ MyFirstMVCApp/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MyFirstMVCApp.Models;
 
 namespace MyFirstMVCApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public ViewResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int begYear, int endYear)
+        {
+            return RedirectToAction("Result", new { begYear, endYear});
+        }
+
+        public ViewResult Result(int begYear, int endYear)
+        {
+            List<TimePerson> listOfPeople = TimePerson.GetPersons(begYear, endYear);
+            return View(listOfPeople);
         }
     }
 }
